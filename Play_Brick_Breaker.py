b091bc683053130e960ac58216dcef487529aff7
JacksonJ01 <jacksonj@hartwick.edu>
2020-05-20 21:16:27 -0400

    For tmw

--- Play_Brick_Breaker.py
+++ Play_Brick_Breaker.py
@@ -1,1 +1,8 @@
+# jackson J.
+# 5.21.2020
+# Taking what I learned yesterday with Play_Pong, I will try to make brick breaker.
+# This will be a lot harder because i will have to find the right window size, and I will have to create a lot of
+# Turtle objects that will be destroyed when the ball hits it#
+from turtle import Turtle
 
+player = Turtle()
